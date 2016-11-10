fd101c71c81b5ffa40926883b839a83a35cfaecf
Nabeel Ahmed <chaudhrynabeelahmed@gmail.com>
2016-11-10 17:25:37 +0500

    edit setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 PACKAGES = ['py_setup_ci', 'py_setup_ci/tests']
 
 # Get version and release info, which is all stored in shablona/version.py
-ver_file = os.path.join('shablona', 'version.py')
-with open(ver_file) as f:
-    exec(f.read())
+# ver_file = os.path.join('shablona', 'version.py')
+# with open(ver_file) as f:
+#    exec(f.read())
 
 opts = dict(name="py_setup_ci",
             description="Python packaging and CI test",
